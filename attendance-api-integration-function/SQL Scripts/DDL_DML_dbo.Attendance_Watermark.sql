bf4611388d62bbbdd1096614550dc35e51b4055d
suchitjoshi1 <suchit.joshi@zinghr.com>
2026-03-17 17:12:15 +0530

    Updated SQL Script

--- attendance-api-integration-function/SQL Scripts/DDL_DML_dbo.Attendance_Watermark.sql
+++ attendance-api-integration-function/SQL Scripts/DDL_DML_dbo.Attendance_Watermark.sql
@@ -4,5 +4,3 @@ CREATE TABLE dbo.Attendance_Watermark
 	[ID] INT IDENTITY(1,1) PRIMARY KEY,
     [last_timestamp] DATETIME
 );
-
---INSERT INTO dbo.Attendance_Watermark(last_timestamp) VALUES ('2026-03-12 16:43:40.813');
