801d907d73a6502612b06b3c2abc8176b550ff2b
suchitjoshi1 <suchit.joshi@zinghr.com>
2026-04-09 15:03:18 +0530

    Onboarding watermark table changes

--- onboarding-api-integration-function/SQL Scripts/DDL_DML_dbo.Onboarding_Watermark.sql
+++ onboarding-api-integration-function/SQL Scripts/DDL_DML_dbo.Onboarding_Watermark.sql
@@ -2,8 +2,9 @@
 CREATE TABLE dbo.onboarding_watermark 
 (
 	[ID] INT IDENTITY(1,1) PRIMARY KEY,
-    [last_timestamp] DATETIME
+    [last_timestamp] DATETIME,
+	[source_db] VARCHAR(50)
 );
 
---Insert Into dbo.onboarding_watermark ([last_timestamp]) Values('2026-03-18 14:39:05.337')
+--Insert Into dbo.onboarding_watermark ([last_timestamp],[source_db]) Values('2026-03-25 12:05:26.973','ELCM_FBINCQA5')
 
